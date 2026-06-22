c5fe2ead4bef0c5528ea1bc76ca073b4240bca93
Abdul Quddous <quddous2369@gmail.com>
2026-06-22 16:48:16 +0500

    Manual data coversion without serializer

--- student_api/api/views.py
+++ student_api/api/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from student.models import Student
 # Create your views here.
 
 def studentview(request):
-    student={
-        'name' : 'abdul',
-        'age': '20'
-    }
-    return JsonResponse(student)
+    student=Student.objects.all()
+    student_list = list(student.values())
+    return JsonResponse(student_list , safe=False)
