85ed075d45a6dca85995174b7eb48bd6971ca303
Abdul Quddous <quddous2369@gmail.com>
2026-06-24 15:28:44 +0500

    Add serializer.py and use to get record

--- student_api/api/views.py
+++ student_api/api/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 from student.models import Student
-# Create your views here.
+from .serializers import StudentSerializer
 
+@api_view(['GET'])
 def studentview(request):
-    student=Student.objects.all()
-    student_list = list(student.values())
-    return JsonResponse(student_list , safe=False)
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
