af33afe56785cce84bd097b4fa03fddc77c4a81c
Abdul Quddous <quddous2369@gmail.com>
2026-06-24 16:02:01 +0500

    ADD Post methos using seriazer adn api view

--- student_api/api/views.py
+++ student_api/api/views.py
@@ -4,8 +4,18 @@ from rest_framework import status
 from student.models import Student
 from .serializers import StudentSerializer
 
-@api_view(['GET'])
+@api_view(['GET' , 'POST'])
 def studentview(request):
-    students = Student.objects.all()
-    serializer = StudentSerializer(students, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == "GET":
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors , status= status.HTTP_400_BAD_REQUEST)
+
+
