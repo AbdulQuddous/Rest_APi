3477e43b4bb27383db23764058884caaae3232e2
Abdul Quddous <quddous2369@gmail.com>
2026-06-26 16:04:41 +0500

    Use mixins to update destroy and retrieve employee data

--- student_api/api/views.py
+++ student_api/api/views.py
@@ -58,30 +58,30 @@ def studentdetail(request , pk):
 #             return Response(serializer.data , status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 
-class Employedetail(APIView):
-    def get_object(self , pk):
-        try:
-            return Employee.objects.get(pk=pk)
-        except:
-            raise Http404
+# class Employedetail(APIView):
+#     def get_object(self , pk):
+#         try:
+#             return Employee.objects.get(pk=pk)
+#         except:
+#             raise Http404
         
-    def get(self,request,pk ):
-        employee = self.get_object(pk)
-        serializer = employeeserializer(employee)
-        return Response(serializer.data , status=status.HTTP_200_OK)
+#     def get(self,request,pk ):
+#         employee = self.get_object(pk)
+#         serializer = employeeserializer(employee)
+#         return Response(serializer.data , status=status.HTTP_200_OK)
     
-    def put(self , request , pk):
-        employee = self.get_object(pk)
-        serializer = employeeserializer(employee, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data , status=status.HTTP_201_CREATED)
-        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+#     def put(self , request , pk):
+#         employee = self.get_object(pk)
+#         serializer = employeeserializer(employee, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data , status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self , request , pk):
-        employee = self.get_object(pk)
-        employee.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self , request , pk):
+#         employee = self.get_object(pk)
+#         employee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class Employees(mixins.ListModelMixin , mixins.CreateModelMixin , generics.GenericAPIView):
     queryset = Employee.objects.all()
@@ -92,3 +92,20 @@ class Employees(mixins.ListModelMixin , mixins.CreateModelMixin , generics.Gener
     
     def post(self ,request):
         return self.create(request)
+    
+class Employedetail(mixins.UpdateModelMixin , mixins.DestroyModelMixin , mixins.RetrieveModelMixin , generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = employeeserializer
+
+    def get(self, request ,pk):
+        return self.retrieve(request , pk)
+    
+    def put(self, request ,pk):
+        return self.update(request , pk)
+    
+    def patch(self, request ,pk):
+        return self.update(request , pk)
+    
+    def delete(self, request ,pk):
+        return self.destroy(request , pk)
+    
